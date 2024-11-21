b1b789f771d15a1cfcf58d1a79d9ee8af3f5abf1
d4nyka <danillesantod@gmail.com>
2024-11-21 11:21:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Taylor Swift',
+    'Quem é Taylor Swift?',
+    'Taylor Swift é uma cantora, compositora, produtora e atriz americana. Ela é conhecida por suas letras autobiográficas e por explorar diversos estilos musicais, como country, pop e indie.'
+)
+
+criaCartao(
+    'Taylor Swift',
+    'Quais são alguns dos álbuns mais famosos de Taylor Swift?',
+    'Alguns dos álbuns mais populares incluem Fearless (2008), 1989 (2014), Reputation (2017), Folklore (2020) e Midnights (2022).'
+)
+
+criaCartao(
+    'Taylor Swift',
+    'Qual foi o primeiro álbum de Taylor Swift?',
+    'O primeiro álbum foi Taylor Swift , lançado em 2006, com músicas focadas no estilo country.'
+)
+
+criaCartao(
+    'Taylor Swift',
+    'Quantos Grammys Taylor Swift já ganhou?',
+    'Até o momento, Taylor Swift ganhou 12 prêmios Grammy, incluindo três Álbuns do Ano, o que é uma das maiores honras da música.'
+)
+criaCartao(
+    'Taylor Swift',
+    'O que é a Taylors Version?',
+    'Taylors Version são regravações de seus primeiros álbuns. Taylor decidiu regravá-los para ter controle sobre suas próprias músicas, depois que os direitos de seus primeiros seis álbuns foram vendidos sem seu consentimento.'
+)
+criaCartao(
+    'Taylor Swift',
+    'Quais são as turnês mais populares de Taylor Swift?',
+    'The Eras Tour (2023), Reputation Stadium Tour (2018) e 1989 World Tour (2015) foram todas bem-sucedidas, com performances de produção grandiosa e setlists repletas de sucessos'
+)
